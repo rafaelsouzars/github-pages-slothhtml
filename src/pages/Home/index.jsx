2e29c611df9fab4df998c8859170daa2b537f80f
rafaelsouzars <rafael21.santos@hotmail.com>
2026-07-08 11:04:54 -0300

    :sparkles: feat(navbar): add menu bar Description: Create menu in navbar. Author: @rafaelsouzars

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,7 +7,7 @@ export function Home() {
 			<a href="https://preactjs.com" target="_blank">
 				<img src={preactLogo} alt="Preact logo" height="160" width="160" />
 			</a>
-			<h1>Get Started building Vite-powered Preact Apps </h1>
+			<h1>Get Started create dotNet CLI apps</h1>
 			<section>
 				<Resource
 					title="Learn Preact"
